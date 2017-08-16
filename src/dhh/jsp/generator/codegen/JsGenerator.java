697dff5149f304e112513815dd6117d10ba54831
suntao <tao.sun@quantgroup.cn>
2017-08-16 16:05:14 +0800

    auto_code_web_1.0

--- src/dhh/jsp/generator/codegen/JsGenerator.java
+++ src/dhh/jsp/generator/codegen/JsGenerator.java
@@ -19,27 +19,12 @@ import static org.mybatis.generator.internal.util.messages.Messages.getString;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
 
-import org.junit.Test;
-import org.mybatis.generator.api.CommentGenerator;
 import org.mybatis.generator.api.FullyQualifiedTable;
 import org.mybatis.generator.api.IntrospectedColumn;
-import org.mybatis.generator.api.dom.java.CompilationUnit;
-import org.mybatis.generator.api.dom.java.Field;
-import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
-import org.mybatis.generator.api.dom.java.JavaVisibility;
-import org.mybatis.generator.api.dom.java.Method;
-import org.mybatis.generator.api.dom.java.Parameter;
-import org.mybatis.generator.api.dom.java.TopLevelClass;
 import org.mybatis.generator.api.dom.xml.TextDocument;
 import org.mybatis.generator.api.dom.xml.TextElement;
-import org.mybatis.generator.codegen.AbstractXmlGenerator;
 import org.mybatis.generator.codegen.XmlConstants;
-import org.mybatis.generator.codegen.mybatis3.xmlmapper.XMLMapperGenerator;
-import org.mybatis.generator.config.JDBCConnectionConfiguration;
-import org.mybatis.generator.internal.util.StringUtility;
 
 import dhh.jsp.generator.element.AbstractTextElementGenerator;
 import dhh.jsp.generator.element.jspContentElementGenerator;
